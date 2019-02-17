c6d92bef650823e0cfcc4a2ee52cda5c0c678ccb
leocavalcante <lc@leocavalcante.com>
2019-02-17 15:55:18 -0300

    Marking Observable abstract and notify required

--- lib/src/observable.dart
+++ lib/src/observable.dart
@@ -1,7 +1,7 @@
 part of observable_state;
 
-class Observable<S, C> {
-  void setState(void Function() update, {C notify}) {
+abstract class Observable<S, C> {
+  void setState(void Function() update, {@required C notify}) {
     update();
     _Observatory.getInstance<S, C>().notify(notify);
   }
