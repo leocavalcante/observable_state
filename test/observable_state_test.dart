b1ed1caba03f701008c695b57e9af74d7f9a7841
leocavalcante <lc@leocavalcante.com>
2019-02-17 18:22:47 -0300

    The counter example as a library test

--- test/observable_state_test.dart
+++ test/observable_state_test.dart
@@ -1,1 +1,63 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:observable_state/observable_state.dart';
+
+enum Changes {
+  increment,
+}
+
+class State extends Observable<State, Changes> {
+  State({int counter = 0}) : _counter = counter;
+
+  int _counter;
+  int get counter => _counter;
+
+  void increment() {
+    setState(() => _counter++, notify: Changes.increment);
+  }
+}
+
+class TestWidget extends StatefulWidget {
+  @override
+  _TestWidgetState createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends StateObserver<TestWidget, State, Changes> {
+  @override
+  List<Changes> get subjects => [Changes.increment];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Text(state.counter.toString()),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: state.increment,
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  testWidgets('Counter example', (tester) async {
+    await tester.pumpWidget(ObservableProvider(
+      State(),
+      child: TestWidget(),
+    ));
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+}
